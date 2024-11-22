de16fe0fe05d2baeca40badc7147e2b5f0f6ef40
Admin <Admin@WIN-0QFN66U68EP>
2024-11-23 00:43:22 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите количество элементов массива: ");
+        int numElements = int.Parse(Console.ReadLine());
+
+        Random random = new Random();
+        int[] array = new int[numElements];
+
+        for (int i = 0; i < numElements; i++)
+        {
+            array[i] = random.Next(-30, 46);
+        }
+
+        for (int i = 0; i < numElements; i++)
+        {
+            Console.Write(array[i] + "\t");
+            if ((i + 1) % 10 == 0)
+            {
+                Console.WriteLine();
+            }
+        }
+
+        Console.WriteLine("\nЭлементы в обратном порядке (игнорируя отрицательные):");
+        for (int i = numElements - 1; i >= 0; i--)
+        {
+            if (array[i] >= 0)
+            {
+                Console.Write(array[i] + " ");
+            }
+        }
+    }
+}
